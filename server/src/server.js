46d84cf788ce6f8fe773426828f9104d6ea50c02
Tanmay Mittal <tanmaymittal@Tanmays-MacBook-Air.local>
2026-05-08 11:11:07 +0530

    Prepare backend for Vercel deployment

--- server/src/server.js
+++ server/src/server.js
@@ -21,6 +21,12 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => {
+    console.log(
+      `Server running on port ${PORT}`
+    );
+  });
+}
+
+export default app;
